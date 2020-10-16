b399e74476f1fa85b26f81669b4cf314a64e0c00
Ivan Moran <ivan_rdm75@hotmail.com>
2020-10-15 19:54:47 -0500

    slices

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
-
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var cantidad int
+	var entero int
+	var s []int
+
+	fmt.Print("Ingrese la cantidad de numeros a sumar: ")
+	fmt.Scan(&cantidad)
+
+	for i := 0; i < cantidad; i++{
+		fmt.Scan(&entero)
+		s = append(s,entero)
+	}
+
+	var res = 0
+
+	for _, num := range s{
+		res += num
+	}
+	fmt.Println(entero)
+	fmt.Println(res)
 }
